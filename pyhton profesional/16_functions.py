5320b390b8117644d11f81523c52daadc42ab1d0
Michael Peralta <michael.peralta@globallogic.com>
2024-01-30 20:05:29 -0300

    add file

---
+++ pyhton profesional/16_functions.py
@@ -0,0 +1,8 @@
+def suma():
+    numero_a = int(input("ingresa un numero entero: "))
+    numero_b = int(input("ingresa otro numero entero: "))
+
+    resultado = numero_a + numero_b
+    print(f'resultado suma: {resultado}')
+
+suma()
